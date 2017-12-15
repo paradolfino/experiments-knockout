abac240d81173aeec8df9070be9af4f5d9ade401
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-14 22:09:13 -0600

    work on observable arrays

--- Beta/index.js
+++ Beta/index.js
@@ -1,14 +1,20 @@
-var userArray1 = ko.observableArray(['Edward', 'Jose', 'Sam']);
+
+function UsersViewModel() {
+    self = this;
+    self.userArray1 = ko.observableArray(['Edward', 'Jose', 'Sam']);
+    self.userArray2 = ko.observableArray([
+        {firstName: 'Edward', lastName: 'Jenkins'},
+        {firstName: 'Percival', lastName: 'Jenkins'}
+    ]);
+}
 
 function output() {
-    $('#output').append('There are ' + userArray1().length + ' users in the array. <br>');
+    $('#output').append('There are ' + userArray1().length + ' users in the array 1. <br>');
+    $('#output').append('There are ' + userArray2().length + ' users in the array 2. <br>');
 }
 
 $(document).ready(function(){
-    output();
-    userArray1().push('Johannas');
-    output();
-    
+    ko.applyBindings(UsersViewModel());
 });
 
 
