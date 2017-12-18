b05ca034efab648de1725b749cb7ef141bd209a5
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 20:15:34 -0600

    delta: style-binding

--- delta/index.js
+++ delta/index.js
@@ -2,7 +2,7 @@
 
 function viewModel() {
     const self = this;
-    self.htmlString("<h1>Lol</h1>");
+    self.htmlString = ko.observable("<h1>Lol</h1>");
     self.amount = ko.observable(11);
 }
 
