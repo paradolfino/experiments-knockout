af1c3a3e2068761c7a25c45e755c32f0a3ce94a9
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 20:32:34 -0600

    delta: foreach, if, ifnot bindings

--- delta/index.js
+++ delta/index.js
@@ -6,6 +6,15 @@ function viewModel() {
     self.amount = ko.observable(11);
     self.url = ko.observable("http://www.google.com");
     self.title = ko.observable("Google");
+
+    //control flow bindings
+    self.employees = ko.observableArray([
+        {id: "e100", name: "Spot"},
+        {id: "e400", name: "Spat"},
+        {id: "e102", name: "Spoon"}
+    ]);
+
+    self.customers = ko.observableArray(["Sprock", "Sprint", "Sprecht"]);
 }
 
 ko.applyBindings(new viewModel());
