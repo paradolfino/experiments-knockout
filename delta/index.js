d5167826e6d37382c031261d22ca908f892ef0ae
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 20:19:48 -0600

    delta: attr-binding

--- delta/index.js
+++ delta/index.js
@@ -4,6 +4,8 @@ function viewModel() {
     const self = this;
     self.htmlString = ko.observable("<h1>Lol</h1>");
     self.amount = ko.observable(11);
+    self.url = ko.observable("http://www.google.com");
+    self.title = ko.observable("Google");
 }
 
 ko.applyBindings(new viewModel());
