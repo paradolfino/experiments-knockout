40161cc1db9eb0073446a11f85c06e0771f8ed6f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-20 21:36:36 -0600

    zeta: add textFadeIn wrapper binding

--- zeta/zeta.js
+++ zeta/zeta.js
@@ -3,13 +3,21 @@ ko.bindingHandlers.changeColor = {
         var value = valueAccessor();
         var valueUnwrapped = ko.unwrap(value);
         $(element).css("color", valueUnwrapped);
-        console.log('test');
     }
 };
+
+ko.bindingHandlers.textFadeIn = { //wrapper binding
+    update: function(element, valueAccessor) {
+        $(element).hide();
+        ko.bindingHandlers.text.update(element, valueAccessor);
+        $(element).fadeIn();
+    }
+}
 
 function viewModel() {
     self = this;
     self.color = ko.observable();
+    self.msg = ko.observable("A text fade in");
 }
 
 ko.applyBindings(new viewModel());
