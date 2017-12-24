a03b53c62a0b9cc9ff32512e56a1c6089d259130
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-23 21:10:59 -0600

    epsilon: change: index.js

--- epsilon/index.js
+++ epsilon/index.js
@@ -32,6 +32,7 @@ function viewModel() {
     self.startTD = function(todo) {
         date = new Date();
         todo.state = 'started';
+        todo.started = date.toLocaleString();
     };
     
     self.doneTD = function(todo) {
