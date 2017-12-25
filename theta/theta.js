b2cad5d5682a42bbc7ea4bd56dee96787b4cfa42
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-24 19:54:09 -0600

    commit to pool[yb7d4i] at 19:54

--- theta/theta.js
+++ theta/theta.js
@@ -10,7 +10,7 @@ viewModel.categoriesAlt = ko.computed(function(){
         {name: 'Food'},
         {name: 'Construction'}
     ]);
-});
+}, viewModel);
 
 $(function(){
     ko.applyBindings(viewModel);
