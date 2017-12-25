9826d929af7c44faf647432d0911b37400dacc5e
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-24 19:49:24 -0600

    commit to pool[yb7d4i] at 19:49

---
+++ theta/theta.js
@@ -0,0 +1,9 @@
+var viewModel = {
+    categories: ko.observableArray([
+        { name: 'Technology', subcategories: ['Programming', 'Hardware', 'Web Development']},
+        { name: 'Business', subcategories: ['Finance', 'Marketing', 'Stocks']},
+        { name: 'Food', subcategories: ['Restaurang', 'Katerer', 'Market']}
+    ])
+}
+
+ko.applyBindings(viewModel);
