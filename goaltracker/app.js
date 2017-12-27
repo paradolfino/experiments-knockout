538f66087b264894fcf0624c289fee81f0ddf816
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 20:26:14 -0600

    commit 5 to pool[brilliant-steaming-rabbit-chu71f] at 20:26 - 26/12/2017

--- goaltracker/app.js
+++ goaltracker/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 var mongojs = require('mongojs');
 var db = mongojs('goaltracker',['goals']);
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client')));
