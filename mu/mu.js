41dde18254fdc306d1b7ba25dc60284ad9980eed
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 20:11:49 -0600

    commit 7 to pool[pretty-shining-river-1eu375] at 20:11 - 26/12/2017

--- mu/mu.js
+++ mu/mu.js
@@ -1,12 +1,13 @@
 ko.components.register('newsletter-widget', {
-    viewModel: function() {
+    viewModel: function(params) {
         this.email = ko.observable();
         this.showMessage = ko.observable(false);
         this.submitForm = function() {
             this.showMessage(true);
         }
     },
-    template: '<h3>Subscribe to our newsletter</h3>\
+    template: 
+    '<h3>Subscribe to our newsletter</h3>\
     <form data-bind="submit:submitForm">\
         <input data-bind="value: email"/>\
         <button type="submit">Submit</button>\
