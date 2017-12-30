05b2ca5c91326fcc855b362c58aa759661d4a37a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-29 20:40:59 -0600

    commit 14 to pool[happy-baking-horses-374cw3] at 20:40 - 29/12/2017

---
+++ goaltracker/client/app.js
@@ -0,0 +1,14 @@
+function getGoals() {
+    $.get('http://localhost:3000/goals', function(data){
+        viewModel.goals(data);
+    });
+}
+
+function viewModel() {
+    var self = this;
+    self.goals = ko.observableArray();
+}
+
+var viewModel = new viewModel();
+
+ko.applyBindings(viewModel);
